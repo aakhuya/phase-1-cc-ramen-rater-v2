58c86047d139d34f34a9f05f8e8b3309a3e88a5c
Adrian Masakhwe Akhuya <akhuya52@gmail.com>
2024-10-17 09:16:30 +0300

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,26 +1,60 @@
-// index.js
-
-// Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  document.querySelector('.detail-image').src = ramen.image;
+  document.querySelector('.name').textContent = ramen.name;
+  document.querySelector('.restaurant').textContent = ramen.restaurant;
+  document.getElementById('rating-display').textContent = ramen.rating;
+  document.getElementById('comment-display').textContent = ramen.comment;
 };
 
 const addSubmitListener = () => {
-  // Add code
-}
+  const form = document.getElementById('new-ramen');
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const newRamen = {
+      name: document.getElementById('new-name').value,
+      restaurant: document.getElementById('new-restaurant').value,
+      image: document.getElementById('new-image').value,
+      rating: document.getElementById('new-rating').value,
+      comment: document.getElementById('new-comment').value
+    };
+
+    const img = document.createElement('img');
+    img.src = newRamen.image;
+    img.alt = newRamen.name;
+
+    img.addEventListener('click', () => handleClick(newRamen));
+
+    document.getElementById('ramen-menu').appendChild(img);
+
+    form.reset();
+  });
+};
 
 const displayRamens = () => {
-  // Add code
+  fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(ramenData => {
+    ramenData.forEach(ramen => {
+      const img = document.createElement('img');
+      img.src = ramen.image;
+      img.alt = ramen.name;
+
+      img.addEventListener('click', () => handleClick(ramen));
+
+      document.getElementById('ramen-menu').appendChild(img);
+    });
+  });
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+  displayRamens();
+  addSubmitListener();
+};
 
 main()
 
-// Export functions for testing
 export {
   displayRamens,
   addSubmitListener,
